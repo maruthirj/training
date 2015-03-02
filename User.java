ae776a28e887a08ce1dfd855944fde89ce1e1f2d
Maruthi <maruthi@vodaplex.com>
2015-03-02 11:18:15 +0530

    User bean

---
+++ User.java
@@ -0,0 +1,63 @@
+package com.mydomain.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable{
+
+	
+	private Integer id;
+	private String name="";
+	
+	private String emailId="";
+	private String password="";
+	
+	private Date joinDate=new Date();
+	private Integer age;
+	private String state="";
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	
+}

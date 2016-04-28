b8f3995afbc0dceed0412120b11911e90730dffe
Maruthi <maruthi.janardhan@gteamstaff.com>
2016-04-28 15:54:33 +0530

    no message

---
+++ CMADEssentials/PartII/app3.js
@@ -0,0 +1,48 @@
+(function(){
+	var app = angular.module('Airlines',[]);
+	app.controller('UserController',function($http, $log, $scope){
+		var controller = this;
+		$scope.users=[];
+		$scope.loading = true;
+		$log.debug("Getting users...");
+		$http.get('rest/user').
+		  success(function(data, status, headers, config) {
+			  $scope.users = data;
+			  $scope.loading = false;
+		  }).
+		  error(function(data, status, headers, config) {
+			  $scope.loading = false;
+			  $scope.error = status;
+		  });
+		
+		$scope.addUser = function(user){
+			$log.debug(user);
+			$.post("rest/user",user,function(data){
+				$log.debug(data);
+				$scope.users.push(user);
+				$scope.$digest();
+			});
+		}
+		$scope.editUser = function(user){
+			console.log(user);
+			$scope.user = user;
+			$scope.showEditForm=true;
+			$scope.showAddForm=false;
+		}
+		
+		$scope.updateUser = function(user){
+			$log.debug(user);
+			$http.put('rest/user',user).
+			  success(function(data, status, headers, config) {
+				  console.log(data);
+				  $scope.showEditForm=false;
+			  }).
+			  error(function(data, status, headers, config) {
+				  $scope.error = status;
+				  $scope.showEditForm=false;
+			  });
+
+		}
+	});
+})();
+
